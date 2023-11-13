b02699026eeea128266319eb1c37ff7b6642fa0c
xo482 <ksh990409@naver.com>
2023-11-13 18:34:30 +0900

    pc에 저장

--- project/demo/src/main/resources/static/myjs.js
+++ project/demo/src/main/resources/static/myjs.js
@@ -1,15 +1,5 @@
-document.getElementById("helloButton").addEventListener("click", function() {
-      fetch("/hello") // 서버의 /hello 엔드포인트로 GET 요청 보내기
-          .then(response => response.text())
-          .then(data => {
-             document.getElementById("response").textContent = data; // 응답 출력
-          })
-          .catch(error => {
-             console.error("Error:", error);
-          });
-      });
-
-document.getElementById("textButton").addEventListener("click", function() {
-      // 외부 index.html 파일을 새 창에서 열기
-      window.open("/external/textSubmit/index.html", "_blank");
-});
+// 버튼 요소를 가져오고 클릭 이벤트 리스너를 추가합니다.
+document.getElementById("goToHello").addEventListener("click", function() {
+    // hello.html로 이동합니다.
+    window.location.href = "hello.html";
+    });
